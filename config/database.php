9c9b0f2aa3f7ed704cb4604f7730f7aaa852c431
TheoFrCh <theo.chiron26@gmail.com>
2021-01-27 08:51:00 +0100

    1.4

---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php
+$user = "theo";
+$pass = "260830";
+
+$dbh = new PDO('mysql:host=localhost;dbname=Blog',$user,$pass);
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once 'config/database.php';
+
+$map =[
+    'home' => 'app/controllers/homeController.php',
+    '404' => 'ressources/views/errors/ERREUR404.php',
+];
+if (filter_has_var(INPUT_GET, 'action')) {
+    $action = filter_input(INPUT_GET,'action', FILTER_SANITIZE_STRING);
+    if(!isset($map ['action'])){
+        $action='404';
+    }
+}
+else {
+    $action='home';
+}
+$fichier = $map [$action];
+include $fichier;
